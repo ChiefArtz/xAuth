93dbad4bbae590cecc8d8c99ba4e62982eb8f3fa
xFlare <boughousp19@student.riverdell.org>
2015-10-18 13:40:20 -0400

    Fixesz

--- src/xFlare/xAuth/Loader.php
+++ src/xFlare/xAuth/Loader.php
@@ -2,27 +2,28 @@
 
 #Loader for xAuth, loads up everything.
 namespace xFlare\FlareHub;
+
 use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 use pocketmine\Server;
-public function onEnable(){
-    public $loginmanager=array(); //Idividual player login statuses using arrays (sessions).
+class Main extends PluginBase implements Listener{
+  public function onEnable(){
+  public $loginmanager=array(); //Idividual player login statuses using arrays (sessions).
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
     $this->getServer()->getLogger()->info("§7> §3Starting up §ax§dAuth§7...§6Loading §edata§7.");
     $this->saveDefaultConfig();
     $this->provider = strtolower($this->getConfig()->get("autentication-type"));
     $this->status = null; //Keeps track of auth status.
     $this->debug = $this->getConfig()->get("debug-mode");
-  }
-  if($this->getConfig()->get("database-checks") === true && $this->provider === "mysql"){
+    if($this->getConfig()->get("database-checks") === true && $this->provider === "mysql"){
       $this->getServer()->getScheduler()->scheduleRepeatingTask(new ErrorChecks($this), 30*20);
+    }
+    $this->checkForConfigErrors($this->getConfig()); //Will check for different config errors.
   }
-  $this->checkForConfigErrors($this->getConfig()); //Will check for different config errors.
-}
-public function checkForConfigErrors($config){ //Will try to fix errors, and repair config to prevent erros further down the rode.
+  public function checkForConfigErrors($config){ //Will try to fix errors, and repair config to prevent erros further down.
     $errors = 0;
     if($this->getConfig()->get("database-checks") === true && $this->provider !== "mysql"){
       $this->getServer()->getLogger()->info("§7[§cError§7] §eInvaild §ax§dAuth §econfig data§7!");
@@ -37,4 +38,3 @@ public function checkForConfigErrors($config){ //Will try to fix errors, and rep
     $this->status = "enabled"; //Assuming errors have been fixed.
 }
     
-
