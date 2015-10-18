69bb33326d1e9e16dcccd0009a11023268b7f718
xFlare <boughousp19@student.riverdell.org>
2015-10-18 16:09:18 -0400

    Update MemoryStatus.php

--- src/xFlare/xAuth/MemoryStatus.php
+++ src/xFlare/xAuth/MemoryStatus.php
@@ -15,7 +15,7 @@ class MemoryTasks implements Listener{
     public function onRun($currentTick){
     	if($this->getServer()->getTicksPerSecond() < 12){
     		$this->owner->getLogger("> Warning, Server is overloaded! xAuth quries may take longer!\n> xAuth will try to reduce it's activity.");
-    		$this->owner->memorymanagerdata++; //The more it adds, the more xAuth will reduce it's inimportant activity.
+    		$this->owner->memorymanagerdata = 1;
     	}
     	elseif($this->memorymanagerdata !== 0){
     		$this->owner->memeorymanagerdata = $this->memeorymanagerdata - 1;
