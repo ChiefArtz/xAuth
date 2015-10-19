145f7f142b19a10d9c89ef6d7f02a94af1271056
xFlare <boughousp19@student.riverdell.org>
2015-10-19 13:42:51 -0400

    xAuth will now start up.

--- src/xFlare/xAuth/MemoryStatus.php
+++ src/xFlare/xAuth/MemoryStatus.php
@@ -3,20 +3,21 @@
 #Helps server stay alive if it is laging.
 namespace xFlare\xAuth;
 
-use pocketmine\scheduler\PluginTask;
 use pocketmine\Server;
-
+use pocketmine\scheduler\PluginTask;
 class MemoryStatus extends PluginTask{
-	public function __construct(Loader $plugin){
+    public function __construct(Loader $plugin){
+        parent::__construct($plugin);
         $this->plugin = $plugin;
+        $this->length = -1;
     }
     public function onRun($currentTick){
         if($this->owner->status === "enabled"){
-    	   if($this->getServer()->getTicksPerSecond() < 12){
+    	   if($this->owner->getServer()->getTicksPerSecond() < 12){
     		  $this->owner->getLogger("> Warning, Server is overloaded! xAuth quries may take longer!\n> xAuth will try to reduce it's activity.");
     		  $this->owner->memorymanagerdata = 1;
     	   }
-           elseif($this->memorymanagerdata !== 0){
+           elseif($this->owner->memorymanagerdata !== 0){
            	$this->owner->memeorymanagerdata = 0;
            }
         }
