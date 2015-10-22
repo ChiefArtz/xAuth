f6483cc85172fa44dc8da162ddcce3cfd4798650
xFlare <boughousp19@student.riverdell.org>
2015-10-22 11:08:14 -0400

    Update QueryTask.php

--- src/xFlare/xAuth/QueryTask.php
+++ src/xFlare/xAuth/QueryTask.php
@@ -1,4 +1,13 @@
 <?php
+/*
+                            _     _     
+            /\             | |   | |    
+ __  __    /  \     _   _  | |_  | |__  
+ \ \/ /   / /\ \   | | | | | __| | '_ \ 
+  >  <   / ____ \  | |_| | | |_  | | | |
+ /_/\_\ /_/    \_\  \__,_|  \__| |_| |_|
+                                        
+                                        */
 
 #Queryies lag less when done on async, so this will do exactly that.
 namespace xFlare\xAuth;
