960a0b71b0c0ae834d00cf69c134f5edbf807d3c
xFlare <boughousp19@student.riverdell.org>
2015-10-22 14:06:14 -0400

    Adding error checks to get ready for mySQL support

--- src/xFlare/xAuth/ErrorChecks.php
+++ src/xFlare/xAuth/ErrorChecks.php
@@ -8,5 +8,20 @@
  /_/\_\ /_/    \_\  \__,_|  \__| |_| |_|
                                         
                                         */
-#Prevents errors, and checks for them (MYSQL).
+#Helps server stay alive if it is laging.
 namespace xFlare\xAuth;
+
+use pocketmine\Server;
+use pocketmine\scheduler\PluginTask;
+class ErrorChecks extends PluginTask{
+    public function __construct(Loader $plugin){
+        parent::__construct($plugin);
+        $this->plugin = $plugin;
+        $this->length = -1;
+    }
+    public function onRun($currentTick){
+       if($this->owner->memorymanagerdata = 1){ //Make sure server is not laging.
+         //Check for errors.
+       }
+    }
+}
