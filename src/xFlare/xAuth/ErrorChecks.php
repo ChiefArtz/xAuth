bc2152b22a9fd1ccc5d84aadb216ebad5f41c41c
xFlare <boughousp19@student.riverdell.org>
2015-10-22 11:08:41 -0400

    Update ErrorChecks.php

--- src/xFlare/xAuth/ErrorChecks.php
+++ src/xFlare/xAuth/ErrorChecks.php
@@ -1,4 +1,12 @@
 <?php
-
+/*
+                            _     _     
+            /\             | |   | |    
+ __  __    /  \     _   _  | |_  | |__  
+ \ \/ /   / /\ \   | | | | | __| | '_ \ 
+  >  <   / ____ \  | |_| | | |_  | | | |
+ /_/\_\ /_/    \_\  \__,_|  \__| |_| |_|
+                                        
+                                        */
 #Prevents errors, and checks for them (MYSQL).
 namespace xFlare\xAuth;
