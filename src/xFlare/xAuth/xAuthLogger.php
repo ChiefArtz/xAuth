d3038f2f0f3d26f7a1bfe3e7259d1065c97e1326
xFlare <boughousp19@student.riverdell.org>
2015-11-09 00:30:40 -0500

    Logger! :D

---
+++ src/xFlare/xAuth/xAuthLogger.php
@@ -0,0 +1,28 @@
+<?php
+/*
+                            _     _     
+            /\             | |   | |    
+ __  __    /  \     _   _  | |_  | |__  
+ \ \/ /   / /\ \   | | | | | __| | '_ \ 
+  >  <   / ____ \  | |_| | | |_  | | | |
+ /_/\_\ /_/    \_\  \__,_|  \__| |_| |_|
+                                        
+                                        */
+namespace xFlare\xAuth;
+
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+use pocketmine\Server;
+/*
+- xAuth runs tasks here to log stuff if enabled. Useful to track bugs.
+*/
+class xAuthLogger implements Listener{
+	public function __construct(Loader $plugin){
+        $this->plugin = $plugin;
+  }
+  public function onWrite(){
+    #Log things
+  }
+}
